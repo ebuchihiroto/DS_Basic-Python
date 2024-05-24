8eb476a84631ce4dd8f25a5a4e54a6c025cb791c
Hiroto <24t2029d@shinshu-u.ac.jp>
2024-05-24 23:34:32 +0900

    finished ex2

--- euclid.py
+++ euclid.py
@@ -2,3 +2,12 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+
+
+def euclid(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+euclid(int(a), int(b))
+print(a)
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,16 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+from math import pi
+a = 0
+b = pi/2
+n=100
+h=(b-a)/n
+x=0
+for i in range(n):
+    a1=a+i*h
+    a2=a+(i+1)*h
+    s=(a2-a1)*(sin(a1)+sin(a2))/2
+    x+=s
+
+print(x)
