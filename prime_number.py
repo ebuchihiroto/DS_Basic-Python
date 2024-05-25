60c27a396c081aa31c824c526f7d8f5c54a23e86
Hiroto <24t2029d@shinshu-u.ac.jp>
2024-05-25 20:04:32 +0900

    last

--- prime_number.py
+++ prime_number.py
@@ -7,6 +7,7 @@ b = input("bの値を入力: ")
 def calc(x):
     if x < 2:
         print("素数ではありません")
+        return
     for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             print("素数ではありません")
