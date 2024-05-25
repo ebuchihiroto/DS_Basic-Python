143d0cf607bb0a5329fcb949f9fa75a11163435f
Hiroto <24t2029d@shinshu-u.ac.jp>
2024-05-25 21:55:14 +0900

    finished ex

--- prime_number.py
+++ prime_number.py
@@ -5,15 +5,23 @@ b = input("bの値を入力: ")
 
 
 def calc(x):
-    if x < 2:
-        print("素数ではありません")
-        return
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
+    try:
+        x = int(x)
+        if x < 0:
+            raise ValueError
+        elif x < 2:
             print("素数ではありません")
-            break
-    else:
-        print("素数です")
+            return
+        for i in range(2, int(x ** 0.5) + 1):
+            if x % i == 0:
+                print("素数ではありません")
+                break
+        else:
+            print("素数です")
 
-calc(int(a))
-calc(int(b))
+    except ValueError:
+        print("正の整数を入力してください")
+
+
+calc(a)
+calc(b)
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,13 +3,24 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
-from math import pi
-a = 0
-b = pi / 2
-n = 100
-h = (b - a) / n
-x = 0
-for i in range(n):
-    x += h * (sin(a + h * i) + sin(a + h * (i + 1))) / 2
+from math import pi , exp
 
-print(x)
+def f(x):
+    return sin(x)
+
+def f2(x):
+    return 4 / (1 + x ** 2)
+
+def f3(x):
+    return (x ** 0.5) * exp( - (x ** 2 ))
+
+def integral(f, a, b, n):
+    h = (b - a) / n
+    result = 0
+    for i in range(n):
+       result += h * (f(a + i * h) + f(a + (i + 1) * h)) / 2
+    return result
+
+print(integral(f,0,(pi/2),50))
+print(integral(f2,0,1,100))
+print(integral(f3,-100,100,1000))
